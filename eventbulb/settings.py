7dfb4e877cd36589300ce11343dedbd006c81293
Dahlia <dahlia@dahlias-air.home>
2022-02-23 20:36:41 +0000

    Set up home page

--- eventbulb/settings.py
+++ eventbulb/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'eventbulb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
